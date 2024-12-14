8251e6770aa8615f303d323fa4423f91b9a25a16
JHeczko <jakub.heczko5@gmail.com>
2024-12-14 17:57:27 +0100

    only image correction

--- Lekcja10/kostki.py
+++ Lekcja10/kostki.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+import random
 
-from numpy.core.defchararray import upper
-from tensorflow.python.ops.gen_data_flow_ops import map_incomplete_size
+from soupsieve.util import lower
+from tensorboard.summary.v1 import image
 
 
 class Application(tk.Frame):
@@ -9,6 +10,12 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.parent = parent
         parent.title(title)
+        parent.geometry("800x800")
+
+        self.text_font = ("System", 20, "bold")
+        self.output_text = tk.StringVar(value="Kliknij guzik na dole, aby rzucić kostką!")
+        self.image_dice = tk.PhotoImage(file="./image/3.png")
+        self.photo_dice = None
 
         self.pack(fill="both", expand=True)
 
@@ -19,22 +26,49 @@ class Application(tk.Frame):
 
         self.createWidgets()
 
+    def random_number(self):
+        self.output_text.set(f"Wylosowałeś: {random.randint(1,6)}")
+        self.image_dice = tk.PhotoImage(file="./image/3.png")
+
+
     def createWidgets(self):
         # Create upper and lower frames
-        upper_label = tk.Frame(self, height=50, bg="#00394d")
+        upper_label = tk.Frame(self, height=50, bg="#262626")
+
         separator = tk.Frame(self, height=5,bg="#005580")
-        lower_label = tk.Frame(self, bg="#00131a", height=800, width=800)
+
+        lower_label = tk.Frame(self, bg="#262626", height=700, width=800)
+        lower_label.rowconfigure(0,minsize=50, weight=1)
+        lower_label.rowconfigure(1,minsize=500, weight=1)
+        lower_label.rowconfigure(2, weight=1)
         lower_label.columnconfigure(0, weight=1)
-        lower_label.columnconfigure(1, weight=1)
 
         # Upper label widgets
-        welcome_text = tk.Label(upper_label,text="Witam w symulatorze kostki 6 ściennej :)", fg="lightblue", font=("System", 20, "bold"))
+        welcome_text = tk.Label(upper_label,text="Witam w symulatorze kostki 6 ściennej :)", fg="lightblue", bg="#262626", font=self.text_font)
         welcome_text.pack(expand=True, fill="both")
 
         # Lower label widgets
+        play_button = tk.Button(
+            lower_label,
+            text="Rzuć kością",
+            font=self.text_font,
+            bg="black",  # Set background color
+            fg="lightblue",  # Set text color
+            activebackground="gray",  # Background when hovered
+            activeforeground="black",  # Text color when hovered
+            highlightthickness=0,  # Remove button border highlight
+            borderwidth=0,  # Adjust border thickness
+            command=self.random_number
+        )
 
+        info_random = tk.Label(lower_label, textvariable=self.output_text, font=self.text_font, fg="lightblue", bg='#262626')
+        self.photo_dice = tk.Label(lower_label,bg="#262626", image=self.image_dice)
 
-        # Place frames in grid
+        info_random.grid(row=0,column=0, sticky="nsew")
+        self.photo_dice.grid(row=1, column=0, sticky="nsew")
+        play_button.grid(row=2, column=0)
+
+        # Setting up grid
         upper_label.grid(row=0, column=0, sticky="ew")
         separator.grid(row=1, column=0, sticky="ew")
         lower_label.grid(row=2, column=0, sticky="nsew")
