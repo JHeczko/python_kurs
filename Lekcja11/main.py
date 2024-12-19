25bbb70f80ba77a449430d30eeda4eb60be89f71
JHeczko <jakub.heczko5@gmail.com>
2024-12-19 13:58:28 +0100

    minor changes 11L

--- Lekcja11/main.py
+++ Lekcja11/main.py
@@ -1,3 +1,20 @@
+class Node:
+    """Klasa reprezentująca węzeł listy jednokierunkowej."""
+
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)   # bardzo ogólnie
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __ne__(self, other):
+            return not self == other
+
+
 class SingleList:
     """Klasa reprezentująca całą listę jednokierunkową."""
 
@@ -40,3 +57,33 @@ class SingleList:
         node.next = None   # czyszczenie łącza
         self.length -= 1
         return node   # zwracamy usuwany node
+
+    def __iter__(self):   # wykorzystanie funkcji generatora
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
+
+    def __del__(self):
+        self.clear()
+
+    # TODO te 3 metody
+    def remove_tail(self): pass   # klasy O(n)
+        # Zwraca cały węzeł, skraca listę.
+        # Dla pustej listy rzuca wyjątek ValueError.
+
+    def join(self, other): pass   # klasy O(1)
+        # Węzły z listy other są przepinane do listy self na jej koniec.
+        # Po zakończeniu operacji lista other ma być pusta.
+
+    def clear(self):
+        self.head = None
+
+if __name__ == '__main__':
+    # Single test
+    slist = SingleList()
+    slist.insert_head(Node(11))  # [11]
+    slist.insert_head(Node(22))  # [22, 11]
+    slist.insert_tail(Node(33))  # [22, 11, 33]
+    for item in slist:  # kolejność 22, 11, 33
+        print(item)
