8d6b87f5d462a46ef00912d6e6e5c7a88ad53d19
JHeczko <jakub.heczko5@gmail.com>
2024-12-21 21:47:32 +0100

    done, working gui

--- Projekt/Okienko.py
+++ Projekt/Okienko.py
@@ -11,18 +11,15 @@ def validate_input(new_value):
         return True
     return False
 
-def generate_random_color():
-    """Generate a random hex color."""
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
-
 
 def create_lab(frame :tk.Frame, rows:int,columns:int):
-    size_block = 20
 
     if type(rows) != int:
         rows = int(rows)
     if type(columns) != int:
         columns = int(columns)
+
+    size_block = 300 / columns
 
     for widget in frame.winfo_children():
         widget.destroy()
@@ -30,9 +27,7 @@ def create_lab(frame :tk.Frame, rows:int,columns:int):
     lab = Labirynt(rows,columns)
     lab.generate_random_labirynt()
     lab_matrix = lab.generate_maze_matrix()
-    print(lab_matrix)
     canvas = tk.Canvas(frame,width=((2*columns)+1) * size_block, height=((rows*2)+1) * size_block)
-    canvas.pack(side=tk.TOP)
 
     for i_row,row in enumerate(lab_matrix):
         for i_col, item in enumerate(row):
@@ -42,15 +37,13 @@ def create_lab(frame :tk.Frame, rows:int,columns:int):
             x2 = x1 + size_block
             y2 = y1 + size_block
             canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
-
-
-
+    canvas.pack(side=tk.TOP)
 
 
 if __name__ == "__main__":
     dark_gray = "#262626"
     darker_gray = "#121211"
-    system_font = ("System", 10, "bold")
+    system_font = ("Arial", 10, "bold")
 
 
     root = tk.Tk()
@@ -78,16 +71,13 @@ if __name__ == "__main__":
     width_in = tk.Entry(frame2, width=20, font = system_font, bg='gray',validate="key", validatecommand=(validate_command, "%P"))
     height_label = tk.Label(frame2, text="Ilość wierszy:", bg=dark_gray, fg="white", font=system_font)
     width_label = tk.Label(frame2, text="Ilość kolumn:", bg=dark_gray, fg="white", font=system_font)
-    button_generate = tk.Button(frame2, text="Wygemeruj Labirynt", bg='gray', fg="white", font=system_font, command=lambda: create_lab(frame1, width_in.get(),height_in.get()))
+    button_generate = tk.Button(frame2, text="Wygeneruj Labirynt", bg='gray', fg="white", font=system_font, command=lambda: create_lab(frame1, width_in.get(),height_in.get()))
 
     height_in.pack(side =tk.BOTTOM, pady=10)
     height_label.pack(side =tk.BOTTOM, pady=10)
     width_in.pack(side =tk.BOTTOM,pady=5)
     width_label.pack(side=tk.BOTTOM, pady=10)
     button_generate.pack(side =tk.TOP,pady=20)
-
-    #creating labirynth for
-
 
 
     root.mainloop()
